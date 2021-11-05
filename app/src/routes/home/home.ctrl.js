1e2788ee6d3c766ff831de6c6cdb66b3cbe7d86e
Yonghoon <yonghoonj98@gmail.com>
2021-11-05 17:37:32 +0900

    로그인 인증 기능 만들기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,42 @@
 "use strict"
 
-const hello = (req, res) => {
-    res.render("home/index");// 기능
+const output = {
+    hello: (req, res) => {
+        res.render("home/index");// 기능
+    },
+    
+    login: (req, res) => {
+        res.render("home/login");
+    },
 };
 
-const login = (req, res) => {
-    res.render("home/login");
-}
+const users = {
+    id: ["test", "go", "back"],
+    psword: ["1234", "1234", "1234"],
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+        
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+    },
+};
 
 module.exports = {
-    hello,
-    login,
-}
+    output,
+    process,
+};
