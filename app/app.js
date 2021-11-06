a4c616074282094b2c0a9cf2d1ef807cfa78f886
Yonghoon <yonghoonj98@gmail.com>
2021-11-06 19:41:47 +0900

    MySQL 연동 & dotenv로 환경변수 등록

--- app/app.js
+++ app/app.js
@@ -18,8 +18,9 @@
 
 // 모듈
 const express = require("express");
-const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
 // 라우팅
 const home = require("./src/routes/home");
@@ -32,6 +33,6 @@ app.use(express.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(express.urlencoded({ extended: true}));
 
-app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
+app.use("/", home); // use -> 미들웨어를 등록해주는 메서드
 
 module.exports = app;
