519a00b4e0cc6d9564195b25a7c69e9e012b210d
Yonghoon <yonghoonj98@gmail.com>
2021-11-06 15:26:49 +0900

    src 실수 삭제 복구

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,44 @@
+"use strict"
+
+const first = require("ee-first");
+
+class UserStorage {
+    static #users = {
+        id: ["test", "go", "back"],
+        psword: ["1234", "1234", "1234"],
+        name: ["우리밋", "나개발", "김팅장"]
+    };
+
+    static getUsers(...fields) {
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+            if (users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        return newUsers;
+    }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
+    }
+
+    static save(userInfo) {
+        const users = this.#users;
+        users.id.push(userInfo.id);
+        users.name.push(userInfo.name);
+        users.psword.push(userInfo.psword);
+        return { success: true };
+    }
+}
+
+module.exports = UserStorage;
