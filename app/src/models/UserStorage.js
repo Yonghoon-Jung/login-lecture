0ab794f3d7c50a5d6fd5473ebb635640137c888c
Yonghoon <yonghoonj98@gmail.com>
2021-12-27 16:29:25 +0900

    winston 프로젝트에 적용

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,36 +1,30 @@
-"use strict"
+"use strict";
 
 const { use } = require("../../app");
 const db = require("../config/db");
 
 class UserStorage {
-        static getUsers(isAll, ...fields) {
-        
-    }
+  static getUsers(isAll, ...fields) {}
 
-    static getUserInfo(id) {
-        return new Promise((resolve, reject) => {
-            const query = "SELECT * FROM users WHERE id = ?;";
-            db.query(query,[id], (err, data) => {    
-                if (err) reject(err);
-                resolve(data[0]);
-            });
-        });
-    }
+  static getUserInfo(id) {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM abc WHERE id = ?;";
+      db.query(query, [id], (err, data) => {
+        if (err) reject(err);
+        else resolve(data[0]);
+      });
+    });
+  }
 
-    static async save(userInfo) {
-        return new Promise((resolve, reject) => {
-            const query = "INSERT INTO users(id, name, psword) VALUES(?, ?, ?);";
-            db.query(
-                query,
-                [userInfo.id, userInfo.name, userInfo.psword],
-                (err) => {    
-                    if (err) reject(`${err}`);
-                    resolve({ success: true });
-                }
-            );
-        });
-    }
+  static async save(userInfo) {
+    return new Promise((resolve, reject) => {
+      const query = "INSERT INTO abc(id, name, psword) VALUES(?, ?, ?);";
+      db.query(query, [userInfo.id, userInfo.name, userInfo.psword], (err) => {
+        if (err) reject(`${err}`);
+        else resolve({ success: true });
+      });
+    });
+  }
 }
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,36 +1,56 @@
-"use strict"
+"use strict";
 
+const logger = require("../../config/logger");
 const User = require("../../models/User");
 
 const output = {
-    hello: (req, res) => {
-        res.render("home/index");// 기능
-    },
-    
-    login: (req, res) => {
-        res.render("home/login");
-    },
+  hello: (req, res) => {
+    logger.info(`GET / 200 "홈 화면으로 이동"`);
+    res.render("home/index"); // 기능
+  },
 
-    register: (req, res) => {
-        res.render("home/register");
-    }
+  login: (req, res) => {
+    logger.info(`GET /login 200 "로그인 화면으로 이동"`);
+    res.render("home/login");
+  },
+
+  register: (req, res) => {
+    logger.info(`GET /register 200 "회원가입 화면으로 이동"`);
+    res.render("home/register");
+  },
 };
 
 const process = {
-    login: async (req, res) => {
-        const user = new User(req.body);
-        const response = await user.login();
-        return res.json(response);
-    },
+  login: async (req, res) => {
+    const user = new User(req.body);
+    const response = await user.login();
+    if (response.err)
+      logger.info(
+        `POST /login 200 Response: "success: ${response.success}, ${response.err}"`
+      );
+    else
+      logger.info(
+        `POST /login 200 Response: "success: ${response.success}, msg: ${response.msg}"`
+      );
+    return res.json(response);
+  },
 
-    register: async (req, res) => {
-        const user = new User(req.body);
-        const response = await user.register();
-        return res.json(response);
-    }
+  register: async (req, res) => {
+    const user = new User(req.body);
+    const response = await user.register();
+    if (response.err)
+      logger.info(
+        `POST /login 200 Response: "success: ${response.success}, ${response.err}"`
+      );
+    else
+      logger.info(
+        `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
+      );
+    return res.json(response);
+  },
 };
 
 module.exports = {
-    output,
-    process,
+  output,
+  process,
 };
